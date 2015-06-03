acec749379ec6e4c1b73ebd5999ddc4a71bd94fe
amitabha <angelo.yin@gmail.com>
2015-06-03 09:51:09 +0800

    refactor to add comments to each functions

--- ch09/note.lisp
+++ ch09/note.lisp
@@ -1,6 +1,7 @@
 (defvar *test-name* nil)
 
 (defun report-result (result form)
+  "Report the results of a single test case. Called by 'check'."
   (format t "~:[FAIL~;PASS~] ...~a: ~a~%" result *test-name* form)
   result)
 
@@ -9,16 +10,21 @@
      ,@body))
 
 (defmacro combine-results (&body forms)
+  "Combine the results (as bolleans) of evaluating 'forms' in order."
   (with-gensyms (result)
     `(let ((,result t))
        ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
        ,result)))
 
 (defmacro check (&body forms)
+  "Run each expression in 'forms' as a  test case"
   `(combine-results
      ,@(loop for f in forms collect `(report-result ,f ',f))))
 
 (defmacro deftest (name parameters &body body)
+  "Define a test function. Within a test function we can call
+  other test functions or use 'check' to ru individual test
+  cases."
   `(defun ,name ,parameters
      (let ((*test-name* (append *test-name* (list ',name))))
        ,@body)))
