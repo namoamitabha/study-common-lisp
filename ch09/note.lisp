e067616dfef51ddb34b13cded214cc34c8336065
amitabha <angelo.yin@gmail.com>
2015-06-03 08:19:51 +0800

    refactor to use check

--- ch09/note.lisp
+++ ch09/note.lisp
@@ -1,8 +1,10 @@
 (defun test-+ ()
-   (report-result (= (+ 1 2 ) 3) '(= (+ 1 2 ) 3))
-   (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
-   (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+   (check (= (+ 1 2 ) 3))
+   (check (= (+ 1 2 3) 6))
+   (check (= (+ -1 -3) -4)))
 
+(defmacro check (form)
+  `(report-result ,form ',form))
 
 (defun report-result (result form)
   (format t "~:[FAIL~;PASS~] ...~a~%" result form))
