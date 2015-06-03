5be110c044a1ae0ff45933df00182bb1e158734b
amitabha <angelo.yin@gmail.com>
2015-06-03 08:40:13 +0800

    refactor to use combine-results

--- ch09/note.lisp
+++ ch09/note.lisp
@@ -1,12 +1,25 @@
+(defun report-result (result form)
+  (format t "~:[FAIL~;PASS~] ...~a~%" result form)
+  result)
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
 (defun test-+ ()
   (check
    (= (+ 1 2 ) 3)
    (= (+ 1 2 3) 6)
    (= (+ -1 -3) -4)))
 
-(defmacro check (&body forms)
-  `(progn
-     ,@(loop for f in forms collect `(report-result ,f ',f))))
 
-(defun report-result (result form)
-  (format t "~:[FAIL~;PASS~] ...~a~%" result form))
