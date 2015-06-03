00a9da7efb2af76649dd14baf4d03187bcc20d4f
amitabha <angelo.yin@gmail.com>
2015-06-03 08:24:02 +0800

    refactor to use check progn

--- ch09/note.lisp
+++ ch09/note.lisp
@@ -1,10 +1,12 @@
 (defun test-+ ()
-   (check (= (+ 1 2 ) 3))
-   (check (= (+ 1 2 3) 6))
-   (check (= (+ -1 -3) -4)))
+  (check
+   (= (+ 1 2 ) 3)
+   (= (+ 1 2 3) 6)
+   (= (+ -1 -3) -4)))
 
-(defmacro check (form)
-  `(report-result ,form ',form))
+(defmacro check (&body forms)
+  `(progn
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
 
 (defun report-result (result form)
   (format t "~:[FAIL~;PASS~] ...~a~%" result form))
