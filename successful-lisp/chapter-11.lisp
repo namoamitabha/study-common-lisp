8dee08d922ec804b42fda68cd84134af3302d364
amitabha <angelo.yin@gmail.com>
2015-11-20 15:06:45 +0800

    chapter-11.lisp

--- successful-lisp/chapter-11.lisp
+++ successful-lisp/chapter-11.lisp
@@ -39,12 +39,49 @@
   (let ((tmp))
     (if (zerop n)
 	(setq tmp (cons nil (rest l)))
-	(setq tmp (cons (car l) (nil-nth (1- n) (rest l))))))
-  (print tmp)
-  tmp)
+	(setq tmp (cons (car l) (nil-nth (1- n) (rest l)))))
+    (print tmp)
+    tmp))
 
 (defparameter *my-list* (list 1 2 3 4))
 (nil-nth 1 *my-list*)
 *my-list*
 ;;********************
+(defparameter list1 (list 1 2 3))
+(defparameter list2 (list 4 5 6))
+(append list1 list2)
+(nconc list1 list2)
 
+;;********************
+(defparameter *my-list* (list 1 2 3 4))
+(rplacd *my-list* (cdr (cdr *my-list*)))
+
+(let ((l (list 1)))
+  (rplacd l l)
+  l)
+
+(let ((l (list 2)))
+  (rplaca l l)
+  l)
+
+;;********************
+(defparameter *my-list* (list 1 2 3 4))
+(delete 3 *my-list*)
+(delete 1 *my-list*)
+
+(defparameter *stack* ())
+(push 3 *stack*)
+(push 2 *stack*)
+(push 1 *stack*)
+(pop *stack*)
+*stack*
+
+;;********************
+(defun stomp-a-constant ()
+  (let ((l '(1 2 3)))
+    (print l)
+    (setf (second l) nil)
+    l))
+
+(stomp-a-constant)
+(stomp-a-constant)
