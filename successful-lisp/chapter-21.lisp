0dc7f5cac824145822aaea538c9f459cf792a37d
amitabha <angelo.yin@gmail.com>
2015-11-27 10:00:22 +0800

    chapter-21.lisp

--- successful-lisp/chapter-21.lisp
+++ successful-lisp/chapter-21.lisp
@@ -105,3 +105,31 @@
 
 (destructuring-sample-3 (1) (3 (4 5)))
 ;;(1 NIL 3 4 5 NIL)
+
+(defmacro with-processes ((name
+			   (pid num-processes)
+			   (work-item work-queue)) &body body)
+  (let ((process-fn (gensym))
+	(items (gensym))
+	(items-lock (gensym)))
+    `(let ((,items (copy-list ,work-queue))
+	   (,items-lock (make-lock)))
+       (flet ((,process-fn (,pid)
+		(let ((,work-item nil))
+		  (loop
+		     (with-lock-grabbed (,items-lock)
+		       (setq ,work-item (pop ,items)))
+		     (when (null ,work-item)
+		       (return))
+		     ,@body))))
+	 (dotimes (i ,num-processes)
+	   (process-run-function
+	    (format nil "~A-~D" ,name i)
+	    #',process-fn
+	    i))))))
+
+(with-processes ("Test"
+		 (id 3)
+		 (item '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)))
+  (format t "~&id ~D item ~A~%" id item)
+  (sleep (random 1.0)))
