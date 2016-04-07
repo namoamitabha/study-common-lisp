a25dc287f9160781b26fe9824239a9687adcaab2
amitabha <angelo.yin@gmail.com>
2016-04-07 13:59:45 +0800

    on-lisp 2.3 2.4

--- on-lisp/chapter-02.lisp
+++ on-lisp/chapter-02.lisp
@@ -29,3 +29,40 @@
 (setf (symbol-function 'double) #'(lambda (x) (* x 2)))
 
 ;;2.3
+(+ 1 2)
+(apply #'+ '(1 2))
+(apply (symbol-function '+) '(1 2))
+(apply #'(lambda (x y) (+ x y)) '(1 2))
+
+(apply #'+ 1 '(2))
+
+(funcall #'+ 1 2)
+
+(mapcar #'(lambda (x) (+ x 10))
+	'(1 2 3))
+
+(mapcar #'+
+	'(1 2 3)
+	'(10 100 1000))
+
+(sort '(1 4 2 5 6 7 3) #'<)
+
+(remove-if #'evenp '(1 2 3 4 5 6 7))
+
+(defun our-remove-if (fn lst)
+  (if (null lst)
+      nil
+      (if (funcall fn (car lst))
+	  (our-remove-if fn (cdr lst))
+	  (cons (car lst) (our-remove-if fn (cdr lst))))))
+
+(our-remove-if #'evenp '(1 2 3 4 5 6 7))
+
+;;2.4
+
+;;(defun behave (animal)
+;;  (funcall (get animal 'behavior)))
+;;(setf (get 'dog 'behavior)
+;;      #'(lambda ()
+;;	  (wag-tail)
+;;	  (bark)))
