f9b0b34d5c803348fcdeaa8d8f3adbd8375046b4
amitabha <angelo.yin@gmail.com>
2016-04-08 15:19:03 +0800

    on-lisp 2.7

--- on-lisp/chapter-02.lisp
+++ on-lisp/chapter-02.lisp
@@ -135,3 +135,27 @@ cities
 
 
 ;;2.7
+(mapcar #'(lambda (x) (+ 2 x))
+	'(2 5 7 3))
+
+(mapcar #'copy-tree '((a b) (c d e)))
+
+(defun list+ (lst n)
+  (mapcar #'(lambda (x) (+ x n))
+	  lst))
+
+(labels ((inc (x) (1+ x)))
+  (inc 3))
+
+(defun count-instances (obj lsts)
+  (labels (
+	   (instances-in (lst)
+	     (if (consp lst)
+		 (+ (if (eq (car lst) obj) 1 0)
+		    (instances-in (cdr lst)))
+		 0)))
+    (mapcar #'instances-in lsts)))
+
+(count-instances 'a '((a b c) (d a r p a) (d a r) (a a)))
+
+;;2.8
