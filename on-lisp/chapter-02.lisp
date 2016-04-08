bde2cc9d8b47d9a29ff863923cc010054ca20a2d
amitabha <angelo.yin@gmail.com>
2016-04-08 14:38:57 +0800

    on-lisp 2.5 2.6

--- on-lisp/chapter-02.lisp
+++ on-lisp/chapter-02.lisp
@@ -66,3 +66,72 @@
 ;;      #'(lambda ()
 ;;	  (wag-tail)
 ;;	  (bark)))
+
+;;2.5
+(let ((y 7))
+  (defun scope-test (x)
+    (list x y)))
+
+(let ((y 5))
+  (scope-test 3))
+
+;;2.6
+(defun list+ (lst n)
+  (mapcar #'(lambda (x) (+ x n))
+	  lst))
+
+(list+ '(1 2 3) 10)
+
+(let ((counter 0))
+  (defun new-id () (incf counter))
+  (defun reset-id () (setq counter 0)))
+
+(new-id)
+(reset-id)
+
+(defun make-adder (n)
+  #'(lambda (x) (+ x n)))
+
+(setq add2 (make-adder 2)
+      add10 (make-adder 10))
+(funcall add2 5)
+(funcall add10 3)
+
+(defun make-adderb(n)
+  #'(lambda (x &optional change)
+      (if change
+	  (setq n x)
+	  (+ x n))))
+
+(setq addx (make-adderb 1))
+(funcall addx 3)
+(funcall addx 100 t)
+(funcall addx 3)
+
+(defun make-dbms (db)
+  (list
+   #'(lambda (key)
+       (cdr (assoc key db)))
+   #'(lambda (key val)
+       (push (cons key val) db)
+       key)
+   #'(lambda (key)
+       (setf db (delete key db :key #'car))
+       key)))
+
+(setq cities (make-dbms '((boston . us) (paris . france))))
+
+(funcall (car cities) 'boston)
+(funcall (second cities) 'london 'england)
+(funcall (car cities) 'london)
+
+(defun lookup (key db)
+  (funcall (car db) key))
+
+(lookup 'london cities)
+(lookup 'boston cities)
+(lookup 'paris cities)
+cities
+
+
+;;2.7
