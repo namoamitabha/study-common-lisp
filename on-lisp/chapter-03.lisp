83a8d5b0d429f702fb3e0821d158a054e245e8af
amitabha <angelo.yin@gmail.com>
2016-04-09 15:37:25 +0800

    on-lisp 31

---
+++ on-lisp/chapter-03.lisp
@@ -0,0 +1,29 @@
+;;2.1 Functional Design
+(defun good-reverse (lst)
+  (labels ((rev (lst acc)
+	         (if (null lst)
+		     acc
+		     (rev (cdr lst) (cons (car lst) acc)))))
+    (rev lst nil)))
+
+(setq lst '(a b c))
+(good-reverse lst)
+
+(reverse lst)
+(setq lst (reverse lst))
+
+(setq lst '(a b c))
+(nreverse lst)
+lst
+
+(truncate 26.21875)
+(= (truncate 26.21875) 26)
+
+(multiple-value-bind (int frac) (truncate 26.21875)
+  (list int frac))
+
+(defun powers (x)
+  (values x (sqrt x) (expt x 2)))
+
+(multiple-value-bind (base root square) (powers 4)
+  (list base root square))
