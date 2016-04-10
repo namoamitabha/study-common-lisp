f7116a38cadc5d38f705022918ad9c6aeb05eb77
amitabha <angelo.yin@gmail.com>
2016-04-10 11:04:30 +0800

    on-lisp 4.3

--- on-lisp/chapter-04.lisp
+++ on-lisp/chapter-04.lisp
@@ -29,4 +29,71 @@
 	    (find2 fn (cdr lst))))))
 (find2 #'bookshops towns)
 
-;;4.3
+;;4.3 Operations on Lists
+(proclaim '(inline last1 single append1 conc1 mklist))
+
+(defun last1 (lst)
+  (car (last lst)))
+
+(defun single (lst)
+  (and (consp lst) (not (cdr lst))))
+
+(defun append1 (lst obj)
+  (append lst (list obj)))
+
+(defun conc1 (lst obj)
+  (nconc lst (list obj)))
+
+(defun mklist (obj)
+  (if (listp obj) obj (list obj)))
+
+(defun longer (x y)
+  (labels ((compare (x y)
+	           (and (consp x)
+			   (or (null y)
+			       (compare (cdr x) (cdr y))))))
+    (if (and (listp x) (listp y))
+	(compare x y)
+	(> (length x) (length y)))))
+
+(defun filter (fn lst)
+  (let ((acc nil))
+    (dolist (x lst)
+      (let ((val (funcall fn x)))
+	(if val (push val acc))))
+    (nreverse acc)))
+(filter #'(lambda (x) (if (numberp x) (1+ x)))
+	  '(a 1 2 b 3 c d 4))
+
+(defun group (source n)
+  (if (zerop n) (error "zero length"))
+  (labels ((rec (source acc)
+	          (let ((rest (nthcdr n source)))
+		    (if (consp rest)
+			(rec rest (cons (subseq source 0 n) acc))
+			(nreverse (cons source acc))))))
+    (rec source nil)))
+(group '(a b c d e f g) 2)
+
+(defun flatten (x)
+  (labels ((rec (x acc)
+	          (cond ((null x) acc)
+			    ((atom x) (cons x acc))
+			    (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
+
+(flatten '(a (b c) ((d e) f)))
+
+(defun prune (test tree)
+  (labels ((rec (tree acc)
+	           (cond ((null tree) (nreverse acc))
+			      ((consp (car tree))
+			       (rec (cdr tree)
+				    (cons (rec (car tree) nil) acc)))
+			      (t (rec (cdr tree)
+				         (if (funcall test (car tree))
+					     acc
+					     (cons (car tree) acc)))))))
+    (rec tree nil)))
+
+(prune #'evenp '(1 2 (3 (4 5) 6) 7 8 (9)))
