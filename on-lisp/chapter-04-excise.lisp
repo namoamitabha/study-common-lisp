6b286939353961165550e0c92dde0bcba1fd901f
amitabha <angelo.yin@gmail.com>
2016-04-10 12:35:54 +0800

    on-lisp 4.3 exercise part1

--- on-lisp/chapter-04-excise.lisp
+++ on-lisp/chapter-04-excise.lisp
@@ -1,4 +1,56 @@
 ;;4.3
-;;last1 single append conc1 mklist
+(proclaim '(inline last1 single append1 conc1 mklist))
+
+(defun last1 (lst)
+  "Return last element value of list"
+  (car (last lst)))
+
+(assert (= (last1 '(1 2 3 4)) 4))
+(assert (eq (last1 nil) nil))
+
+
+(defun single (lst)
+  "Whether list contains only one element"
+  (if (null lst)
+      nil
+      (and (car lst) (null (cdr lst)))))
+
+(assert (eq (single '(1 2 3 4)) nil))
+(assert (eq (single nil) nil))
+(assert (single '(1)))
+
+
+(defun append1 (lst obj)
+  "Attach a new element to the end of a list"
+  (if (consp obj)
+      (append lst obj)
+      (append lst (list obj))))
+
+(assert (equal (append1 '(1 2 3 4) 4) '(1 2 3 4 4)))
+(assert (equal (append1 nil 4) '(4)))
+(assert (equal (append1 '(1 2 3 4) '(4)) '(1 2 3 4 4)))
+
+
+(defun conc1 (lst obj)
+  "Attach a new element to the end of a list destructively"
+  (if (listp obj)
+      (nconc lst obj)
+      (nconc lst (list obj))))
+
+(assert (equal (conc1 '(1 2 3) 4) '(1 2 3 4)))
+(assert (equal (conc1 nil '(1)) '(1)))
+(assert (equal (conc1 '(1 2 3) '(4)) '(1 2 3 4)))
+
+
+(defun mklist (obj)
+  "Ensure obj is list"
+  (if (consp obj)
+      obj
+      (list obj)))
+
+(assert (listp (mklist 'a)))
+(assert (listp (mklist '(a b))))
+(assert (listp (mklist nil)))
+
 
 ;;longer filter group flatten prune
