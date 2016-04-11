286deadb5d8b04e30cc014a9a69a6938f8a577e2
amitabha <angelo.yin@gmail.com>
2016-04-11 11:37:14 +0800

    on-lisp 4.3 refactor code format

--- on-lisp/chapter-04-excise.lisp
+++ on-lisp/chapter-04-excise.lisp
@@ -57,12 +57,12 @@
 (defun longer (x y)
   "Compares two sequence and returns true if x is longer"
   (labels ((rec (x y)
-	          (and (consp x)
-		       (or (null y)
-			     (rec (cdr x) (cdr y))))))
+             (and (consp x)
+                  (or (null y)
+                      (rec (cdr x) (cdr y))))))
     (if (and (listp x) (listp y))
-	(rec x y)
-	(> (length x) (length y)))))
+        (rec x y)
+        (> (length x) (length y)))))
 
 (assert (not (longer '(1 2) '(4 5 6))))
 (assert (longer '(4 5 6) '(1 2)))
@@ -72,28 +72,28 @@
 (defun filter (fn lst)
   "Returns what some would have returned for successive cdrs of the list"
   (labels ((rec (lst acc)
-	     (cond ((null lst) acc)
-		   ((atom lst)
-		    (let ((val (funcall fn lst)))
-		      (format t "~A" val)
-		      (if val (push val acc))))
-		   (t
-		    (rec (cdr lst) (rec (car lst) acc)))
-		   )))
+             (cond ((null lst) acc)
+                   ((atom lst)
+                    (let ((val (funcall fn lst)))
+                      (format t "~A" val)
+                      (if val (push val acc))))
+                   (t
+                    (rec (cdr lst) (rec (car lst) acc)))
+                   )))
     (rec lst nil)))
 
 (assert (equal (filter #'(lambda (x) (if (numberp x) (1+ x)))
-	       '(a 1 2 b 3 c d 4))
-	       '(2 3 4 5)))
+                       '(a 1 2 b 3 c d 4))
+               '(2 3 4 5)))
 
 (defun group (source n)
   "Group list source to sublist with length as n, left put to last sublist"
   (if (zerop n) (error "zero length"))
   (labels ((rec (source acc)
-	          (let ((rest (nthcdr n source)))
-		    (if (consp rest)
-			(rec rest (cons (subseq source 0 n) acc))
-			(nreverse (cons source acc))))))
+             (let ((rest (nthcdr n source)))
+               (if (consp rest)
+                   (rec rest (cons (subseq source 0 n) acc))
+                   (nreverse (cons source acc))))))
     (if source (rec source nil) nil)))
 
 (assert (equal (group '(a b c d e f g) 2) '((a b) (c d) (e f) (g))))
@@ -104,9 +104,9 @@
 (defun flatten (x)
   "Returns a list of all the atoms that are elements of a list or elements of its elements, and so on"
   (labels ((rec (x acc)
-	          (cond ((null x) acc)
-			    ((atom x) (cons x acc))
-			    (t (rec (car x) (rec (cdr x) acc))))))
+             (cond ((null x) acc)
+                   ((atom x) (cons x acc))
+                   (t (rec (car x) (rec (cdr x) acc))))))
     (rec x nil)))
 
 (assert (equal (flatten '(a (b c) ((d e) f))) '(a b c d e f)))
@@ -118,15 +118,15 @@
 (defun prune (test tree)
   "To remove-if as copy-tree is to copy-list. Recurses down to sublists"
   (labels ((rec (tree acc)
-	          (cond ((null tree) (nreverse acc))
-			    ((consp (car tree))
-			     (rec (cdr tree)
-				     (cons (rec (car tree) nil) acc)))
-			    (t (rec (cdr tree)
-				       (if (funcall test (car tree))
-					   acc
-					   (cons (car tree) acc)))))))
+             (cond ((null tree) (nreverse acc))
+                   ((consp (car tree))
+                    (rec (cdr tree)
+                         (cons (rec (car tree) nil) acc)))
+                   (t (rec (cdr tree)
+                           (if (funcall test (car tree))
+                               acc
+                               (cons (car tree) acc)))))))
     (rec tree nil)))
 
 (assert (equal (prune #'evenp '(1 2 (3 (4 5) 6) 7 8 (9)))
-	                '(1 (3 (5)) 7 (9))))
+               '(1 (3 (5)) 7 (9))))
