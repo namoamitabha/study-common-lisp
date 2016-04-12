fb4cb48835dee306626719c6cb59f16251fb22f3
amitabha <angelo.yin@gmail.com>
2016-04-12 16:13:04 +0800

    on-lisp 5.2

--- on-lisp/chapter-05.lisp
+++ on-lisp/chapter-05.lisp
@@ -17,3 +17,17 @@
 (assert (equal (remove-if (complement #'oddp) '(1 2 3 4 5 6))
                '(1 3 5)))
 
+;;5.2 Orthogonality
+(defvar *!equive* (make-hash-table))
+
+(defun ! (fn)
+  (or (gethash fn *!equive*) fn))
+
+(defun def! (fn fn!)
+  (setf (gethash fn *!equive*) fn!))
+
+(def! #'remove-if #'delete-if)
+
+(let ((lst '(1 2 3 4)))
+  (equal (delete-if #'oddp lst)
+         (funcall (! #'remove-if) #'oddp lst)))
