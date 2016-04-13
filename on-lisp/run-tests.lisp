9d65f07431b0faf5fe92e8744285d2b067417241
amitabha <angelo.yin@gmail.com>
2016-04-13 10:34:32 +0800

    on-lisp add lisp-unit

---
+++ on-lisp/run-tests.lisp
@@ -0,0 +1,20 @@
+#! /usr/bin/sbcl --script
+
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+				       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+(load "lisp-unit")
+;;(ql:quickload :lisp-unit)
+
+(use-package :lisp-unit)
+
+(setq *print-summary* t)
+(setq *print-failures* t)
+(setq *print-errors* t)
+
+(load "chapter-05.lisp")
+(load "chapter-05.tests.lisp")
+(run-tests)
+
