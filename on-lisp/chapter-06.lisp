de207c947da2677a441c0f58a344def4e288f22f
amitabha <angelo.yin@gmail.com>
2016-04-14 15:42:58 +0800

    on-lisp 6.2

--- on-lisp/chapter-06.lisp
+++ on-lisp/chapter-06.lisp
@@ -36,3 +36,56 @@
 ;; Is the coin a penny?
 ;; >> yes
 ;; LINCOLN
+
+;;6.2 Compiling Networks
+(defvar *nodes* (make-hash-table))
+
+(defun defnode (name conts &optional yes no)
+  (setf (gethash name *nodes*)
+        (if yes
+            #'(lambda ()
+                (format t "~A~%>> " conts)
+                (case (read)
+                  (yes (funcall (gethash yes *nodes*)))
+                  (t (funcall (gethash no *nodes*)))))
+            #'(lambda () conts))))
+
+;; (funcall (gethash 'people *nodes*))
+;; Is the person a man?
+;; >> yes
+;; Is he living?
+;; >> no
+;; Was he American?
+;; >> yes
+;; Is he on a coin?
+;; >> yes
+;; Is the coin a penny?
+;; >> yes
+;; LINCOLN
+
+;;TODO: compile-net doesn't work
+(defvar *nodes* nil)
+
+(defun defnode (&rest args)
+  (push args *nodes*)
+  args)
+
+(defun compile-net (root)
+  (let ((node (assoc root *nodes*)))
+    (if (null node)
+        nil
+        (let ((conts (second node))
+              (yes (third node))
+              (no (fourth node)))
+          (if yes
+              (let ((yes-fn (compile-net yes))
+                    (no-fn (compile-net no)))
+                #'(lambda ()
+                    (format t "~A~%>> " conts)
+                    (funcall (if (eq (read) 'yes)
+                                 yes-fn
+                                 no-fn))))
+              #'(lambda () conts))))))
+
+(setq n (compile-net 'people))
+(funcall n)
