33415fe3d5b89711356c0938cfa55cf569869215
amitabha <angelo.yin@gmail.com>
2016-04-18 15:53:38 +0800

    on-lisp 7.6 test

--- on-lisp/chapter-07.lisp
+++ on-lisp/chapter-07.lisp
@@ -113,3 +113,20 @@
        ,@body)))
 
 ;;7.6 A Model of Macros
+(defmacro our-expander (name)
+  `(get ,name 'expander))
+
+(defun our-macroexpand-1 (expr)
+  (if (and (consp expr) (our-expander (car expr)))
+      (funcall (our-expander (car expr)) expr)
+      expr))
+
+(defmacro our-defmacro (name parms &body body)
+  (let ((g (gensym)))
+    `(progn
+       (setf (our-expander ',name)
+             #'(lambda (,g)
+                 (block ,name
+                   (destructuring-bind ,parms (cdr ,g)
+                     ,@body))))
+       ',name)))
