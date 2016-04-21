b4535b821d3b6d22a9ce9408cdfbc52292dac83e
amitabha <angelo.yin@gmail.com>
2016-04-21 15:29:50 +0800

    successful-lisp add commonqt example

--- successful-lisp/chapter-26.lisp
+++ successful-lisp/chapter-26.lisp
@@ -1,3 +1,9 @@
-;;Put on a Happy Face:Interface Builders
+;;chapter 26 Put on a Happy Face:Interface Builders
 ;;CLIM
 ;;Garnet
+
+;;ref:
+;;Building a Common Lisp GUI with CommonQt
+;;https://gorkovchanin.wordpress.com/2015/05/02/building-a-common-lisp-gui-with-commonqt/
+;;commonqt example:
+;;https://github.com/namoamitabha/study-common-lisp/tree/master/commonqt-example
