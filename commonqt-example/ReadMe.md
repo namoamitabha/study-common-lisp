20b119d180d582fbb94e259b69450e17b840da27
amitabha <angelo.yin@gmail.com>
2016-04-21 10:11:08 +0800

    CommonQT example helloname

--- commonqt-example/ReadMe.md
+++ commonqt-example/ReadMe.md
@@ -1,13 +1,15 @@
-;;Precondition:  
-;;install qt on debian  
-;;https://common-lisp.net/project/commonqt/  
-;;sudo apt-get install libqt4-dev libsmokeqtgui4-3 libsmokeqt4-dev  
+Precondition:  
+install qt on debian  
+https://common-lisp.net/project/commonqt/  
+sudo apt-get install libqt4-dev libsmokeqtgui4-3 libsmokeqt4-dev  
 
 
-;;CommonQt Tutorial #1: Hello World  
-;;https://cheryllium.wordpress.com/2014/02/22/commonqt-tutorial-1/  
-;;install commonqt  
-;;source: helloworld.lisp  
+CommonQt Tutorial #1: Hello World  
+https://cheryllium.wordpress.com/2014/02/22/commonqt-tutorial-1/  
+install commonqt  
+source: helloworld.lisp
 
 
-
+CommonQt Tutorial #2: Hello Name  
+https://cheryllium.wordpress.com/2014/03/03/commonqt-tutorial-2/  
+Source: helloname.lisp  
---
+++ commonqt-example/helloname.lisp
@@ -0,0 +1,40 @@
+(ql:quickload :qt)
+(in-package :qt)
+(named-readtables:in-readtable :qt)
+
+(defclass hello-name-app ()
+  ((name-edit :accessor name-edit)
+   (name-button :accessor name-button)
+   (name-label :accessor name-label))
+  (:metaclass qt-class)
+  (:qt-superclass "QWidget")
+  (:slots ("show-name()" show-name)))
+
+(defmethod initialize-instance :after
+    ((instance hello-name-app) &key)
+  (new instance)
+  (init-ui instance))
+
+(defmethod init-ui ((instance hello-name-app))
+  (#_setGeometry instance 400 400 300 300)
+  (#_setWindowTitle instance "Hello Name!")
+  (setf (name-edit instance) (#_new QLineEdit "???" instance)
+        (name-button instance) (#_new QPushButton "Click!" instance)
+        (name-label instance) (#_new QLabel "" instance))
+  (#_move (#_new QLabel "What is your name?" instance) 10 10)
+  (#_move (name-edit instance) 10 40)
+  (#_move (name-button instance) 10 70)
+  (#_move (name-label instance) 10 100)
+  (#_setMinimumWidth (name-label instance) 270)
+  (connect (name-button instance) "clicked()" instance "show-name()"))
+
+(defmethod show-name ((instance hello-name-app))
+  (let ((name (#_text (name-edit instance))))
+    (#_setText (name-label instance)
+               (if (and name (string/= name ""))
+                   (concatenate 'string "Hello, "
+                                (princ-to-string name))
+                   "Error"))))
+
+(make-qapplication)
+(with-main-window (window (make-instance 'hello-name-app)))
