77974b9ae8d128c80d6895714390714e4a7f7150
amitabha <angelo.yin@gmail.com>
2016-04-27 15:19:58 +0800

    pcl-ch11 vectors

--- pcl/ch11/ch11-tests.lisp
+++ pcl/ch11/ch11-tests.lisp
@@ -8,41 +8,25 @@
   (assert-true t)
   (assert-true (my-true)))
 
-(vector 1)
-
-(make-array 5 :initial-element nil)
-
-(defparameter *x* (make-array 5 :fill-pointer 0))
-
-(vector-push 'a *x*)
-
-(vector-pop *x*)
-
-(defparameter *y* (make-array 2 :fill-pointer 0 :adjustable t))
-
-(vector-push 'a *y*)
-
-(vector-push 'a *y*)
-
-(vector-push 'a *y*)
-
-(vector-push-extend 'a *y*)
-
-(vector-push-extend 'a *y*)
-
-(vector-push-extend 'a *y*)
-
-(make-array 5 :fill-pointer 0 :adjustable t :element-type 'character)
-
-(defparameter *str* (make-array 5 :fill-pointer 0 :adjustable t :element-type 'character))
-
-(vector-push #\c *str*)
-
-
-(make-array 5 :fill-pointer 0 :adjustable t :element-type 'bit)
-
-(defparameter *bit* (make-array 5 :fill-pointer 0 :adjustable t :element-type 'bit))
-
-(vector-push '1 *bit*)
-
-(defparameter *x* (vector 1 2 3))
+(define-test test-vector
+    (:tag :unittest)
+  (assert-equality #'equalp
+                   #(1 2 3 4 5)
+                   (vector 1 2 3 4 5))
+  (assert-equality #'equalp
+                   #(nil nil nil nil nil)
+                   (make-array 5 :initial-element nil))
+  ;; (make-array 5 :fill-pointer 0) not real resizable
+  (let ((array (make-array 5 :fill-pointer 0)))
+    (dotimes (x 6 t)
+      (vector-push 'a array))
+    (assert-equal 5 (length array)))
+  ;; :adjustable
+  ;; to use vector-push-extend
+  (let ((array-adjustable (make-array 2 :fill-pointer 0 :adjustable t)))
+    (dotimes (x 6 t)
+      (print (length array-adjustable))
+      (if (> 2 (length array-adjustable))
+          (vector-push 'a array-adjustable)
+          (vector-push-extend 'a array-adjustable)))
+    (assert-equal 6 (length array-adjustable))))
