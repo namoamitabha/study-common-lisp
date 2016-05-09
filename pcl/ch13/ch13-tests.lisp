b37699b5bb6ef1579992eec3f0f958e267f4214b
amitabha <angelo.yin@gmail.com>
2016-05-09 09:36:24 +0800

    pcl-ch13 test-Sets

--- pcl/ch13/ch13-tests.lisp
+++ pcl/ch13/ch13-tests.lisp
@@ -7,3 +7,26 @@
     (:tag :unittest)
   (assert-true t)
   (assert-true (my-true)))
+
+(define-test test-Trees
+    (:tag :unittest)
+  (assert-equality #'equalp
+                   '(10 2 (3 2 10) ((10 10) (2 2)))
+                   (subst 10 1 '(1 2 (3 2 1) ((1 1) (2 2))))))
+
+(defparameter *set* ())
+(define-test test-Sets
+    (:tag :unittest)
+  (adjoin 1 *set*)
+  (assert-false *set*)
+  (setf *set* (adjoin 1 *set*))
+  (assert-equality #'equalp
+                   '(1)
+                   *set*)
+  (pushnew 2 *set*)
+  (pushnew 2 *set*)
+  (assert-equality #'equalp
+                   '(2 1)
+                   *set*)
+  (assert-true (subsetp '(3 2 1) '(1 2 3 4)))
+  (assert-false (subsetp '(1 2 3 4) '(3 2 1))))
