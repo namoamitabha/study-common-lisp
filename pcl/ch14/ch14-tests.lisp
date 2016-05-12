da4271bfd8aba4497e738b5845a104a6838ae2a1
amitabha <angelo.yin@gmail.com>
2016-05-12 09:44:06 +0800

    pcl-ch14 test-Constructing-New-Pathnames

--- pcl/ch14/ch14-tests.lisp
+++ pcl/ch14/ch14-tests.lisp
@@ -25,3 +25,29 @@
                 (directory-namestring #P"/foo/bar/baz.txt"))
   (assert-equal "baz.txt"
                 (file-namestring #P"/foo/bar/baz.txt")))
+
+(define-test test-Constructing-New-Pathnames
+    (:tag :unittest)
+  (assert-true (make-pathname :directory '(:absolute "foo" "bar")
+                              :name "baz"
+                              :type ".txt"))
+  (assert-equal #P"/foo/bar/baz.txt"
+                (make-pathname :device "c"
+                               :directory '(:absolute "foo" "bar")
+                               :name "baz"
+                               :type "txt"))
+  (assert-equal #P"backups/baz.txt"
+                (make-pathname :directory '(:relative "backups")
+                               :defaults #p"/foo/bar/baz.txt"))
+  (assert-equal #P"/www/html/foo/bar.html"
+                (merge-pathnames #p"foo/bar.html" #p"/www/html/"))
+  (assert-equal #P"html/foo/bar.html"
+                (merge-pathnames #p"foo/bar.html" #p"html/"))
+  (assert-equal "html/foo/bar.html"
+                (enough-namestring #p"/www/html/foo/bar.html" #p"/www/"))
+  (assert-equal #P"www-backups/html/foo/bar.html"
+                (merge-pathnames
+                 (enough-namestring #p"/www/html/foo/bar.html" #p"/www/")
+                 #p"www-backups/"))
+  (assert-equal #P"foo.txt"
+                (make-pathname :name "foo" :type "txt")))
