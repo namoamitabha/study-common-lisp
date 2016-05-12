f449c22942e99c24d5012f94217387993080170f
amitabha <angelo.yin@gmail.com>
2016-05-12 09:22:50 +0800

    pcl-ch14 test-How-Pathnames-Represent-Filenames

--- pcl/ch14/ch14-tests.lisp
+++ pcl/ch14/ch14-tests.lisp
@@ -1,4 +1,4 @@
-(defpackage pcl-ch14-tests
+             (defpackage pcl-ch14-tests
   (:use :common-lisp :pcl-ch14 :lisp-unit))
 
 (in-package :pcl-ch14-tests)
@@ -7,3 +7,21 @@
     (:tag :unittest)
   (assert-true t)
   (assert-true (my-true)))
+
+(define-test test-How-Pathnames-Represent-Filenames
+    (:tag :unittest)
+  (let ((pathname "/foo/bar/baz.txt"))
+    (assert-equal '(:ABSOLUTE "foo" "bar")
+                  (pathname-directory pathname))
+    (assert-equal "baz"
+                  (pathname-name pathname))
+    (assert-equal "txt"
+                  (pathname-type pathname)))
+  (assert-equal #P"/foo/bar/baz.txt"
+                (pathname "/foo/bar/baz.txt"))
+  (assert-equal "/foo/bar/baz.txt"
+                (namestring #P"/foo/bar/baz.txt"))
+  (assert-equal "/foo/bar/"
+                (directory-namestring #P"/foo/bar/baz.txt"))
+  (assert-equal "baz.txt"
+                (file-namestring #P"/foo/bar/baz.txt")))
